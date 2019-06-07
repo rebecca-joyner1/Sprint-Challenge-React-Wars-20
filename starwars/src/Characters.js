344400b2e89b40580b95ae381d8df5da281e7aef
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-06-07 12:07:27 -0400

    fix

--- starwars/src/Characters.js
+++ starwars/src/Characters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './StarWars.css';
+import './components/StarWars.css';
 
 
 
--- starwars/src/StarWarsMap.js
+++ starwars/src/StarWarsMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './StarWars.css';
+import './components/StarWars.css';
 
 import Characters from './Characters';
 
